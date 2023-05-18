efe8a01a21a2454fbfb3f4f0c26fb7b0b8a57d6a
parkhyeonji <iamphj3@naver.com>
2023-05-18 16:25:10 +0900

    design : 투표 UI 구현

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,4 @@
 export { ReactComponent as IcLeftSmall } from './ic_left_small.svg';
 export { ReactComponent as IcMainIcon } from './ic_mainIcon.svg';
 export { ReactComponent as IcLine } from './ic_line.svg';
+export { ReactComponent as IcBlueLine } from './ic_blueline.svg';
--- src/components/Invitation/Vote.jsx
+++ src/components/Invitation/Vote.jsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from '../common/Header';
+import { IcBlueLine } from '../../assets/icons';
+import styled from 'styled-components';
 
 const Vote = () => {
-  return <div></div>;
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <StVoteWrapper>
+      <Header headerName='장소 투표하기' />
+      <h2>담주에 돼지파티 할사람</h2>
+      <IcBlueLine />
+      <p>애들아 장소 투표해줘~~</p>
+      <StVoteOption>
+        <label className={selectedOption === 'option1' ? 'selected' : ''}>
+          홍대
+          <input
+            type='radio'
+            value='option1'
+            checked={selectedOption === 'option1'}
+            onChange={handleOptionChange}
+          />
+        </label>
+        <br />
+        <label className={selectedOption === 'option2' ? 'selected' : ''}>
+          건대
+          <input
+            type='radio'
+            value='option2'
+            checked={selectedOption === 'option2'}
+            onChange={handleOptionChange}
+          />
+        </label>
+        <br />
+        <label className={selectedOption === 'option3' ? 'selected' : ''}>
+          냥대
+          <input
+            type='radio'
+            value='option3'
+            checked={selectedOption === 'option3'}
+            onChange={handleOptionChange}
+          />
+        </label>
+      </StVoteOption>
+    </StVoteWrapper>
+  );
 };
 
 export default Vote;
+
+const StVoteWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > h2 {
+    margin-top: 2rem;
+    color: #589bff;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 148%;
+  }
+
+  & > svg {
+    margin-top: 0.8rem;
+  }
+
+  & > p {
+    margin-top: 3rem;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 150%;
+  }
+`;
+
+const StVoteOption = styled.div`
+  width: 29.6rem;
+  margin-top: 1.9rem;
+
+  & > label {
+    display: flex;
+    justify-content: space-between;
+
+    width: 100%;
+    height: 4.8rem;
+    padding: 1.4rem 2rem;
+
+    border: 0.1rem solid #d0d4da;
+    border-radius: 0.8rem;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 148%;
+
+    &.selected {
+      border: 0.1rem solid #589bff;
+    }
+  }
+`;
