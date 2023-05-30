bff2eef5c5f8de7042345d5621ef0356583666fd
parkhyeonji <iamphj3@naver.com>
2023-05-31 05:29:15 +0900

    chore : promise import 수정

--- src/pages/PromisePage.jsx
+++ src/pages/PromisePage.jsx
@@ -1,17 +1,7 @@
-import Promise from "../components/promise/Promise";
-import { useNavigate } from "react-router";
+import Promise from '../components/promise/Promise';
 
 const PromisePage = () => {
-  const handleClick = (e) => {
-    const navigate = useNavigate();
-    navigate("/promise/info", { state: e.target.value });
-  };
-
-  return (
-    <>
-      <Promise />
-    </>
-  );
+  return <Promise />;
 };
 
 export default PromisePage;
